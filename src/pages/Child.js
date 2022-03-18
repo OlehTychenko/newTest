2cd878e9ba8aae52e8999d3c2c80fcfe6f91ce93
otych <otych@softserveinc.com>
2022-03-18 15:09:36 +0200

    destruct props

--- src/pages/Child.js
+++ src/pages/Child.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export class Child extends React.Component {
     render() {
+        const { username, name } = this.props.user;
         return (
         <>
             <tr onClick={this.props.onSelectUser} style={{backgroundColor: this.props.isUserSelected ? "yellow" : "white"}}>
-                <td>{this.props.user.username}</td>
-                <td>{this.props.user.name}</td>
+                <td>{username}</td>
+                <td>{name}</td>
             </tr>
         </>
         )
